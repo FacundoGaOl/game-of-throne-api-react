f083bcba8c58f536e35071e7963e75c26124edea
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2026-02-08 21:42:59 +0100

    feat added correct card structure and size

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,9 @@
 const Card = ({character}) => {
     return (
+    <section className="max-w-[1200px]">
         <div className="flex flex-col items-center">
-            <div>
-                <img src={character.imageUrl} alt={`Retrato de ${character.firstName}`}/>
-            </div>
-            <div>
+            <img className="w-full h-auto blockaspect-[3/4] object-cover mb-2.5 rounded-md" src={character.imageUrl} alt={`Retrato de ${character.firstName}`}/>
+            <div className="flex justify-between">
                 <div className="text-center">
                     <h2 className="text-center text-white text-bold">
                         {character.fullName}
@@ -16,11 +15,12 @@ const Card = ({character}) => {
                         {character.title}
                     </h4>
                 </div>
-                <div>
-
+                <div className="max-w-3/10">
+                    <img className="" src="src/assets/images/houseStark.png" alt="" />
                 </div>
             </div>
         </div>
+    </section>
     );
 };
 export default Card;
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -15,14 +15,16 @@ const Characters = () => {
 
     return (
         <>
-            <h1 className="text-3xl font-bold text-center mt-20">
-                Personajes
-            </h1>
-            <ul className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-8 px-10">
-                {characters.map(character => (
-                <Card key={character.id} character={character}/>
-                ))}
-            </ul>
+            <section className="max-w-[1200px] w-full mx-auto px-4">
+                <h1 className="text-3xl font-bold text-center mt-20 text-white">
+                    Personajes
+                </h1>
+                <ul className="grid grid-cols-1 md:grid-cols-4 gap-8 mt-8 px-10">
+                    {characters.map(character => (
+                    <Card key={character.id} character={character}/>
+                    ))}
+                </ul>
+            </section>
         </>
     )
 }
