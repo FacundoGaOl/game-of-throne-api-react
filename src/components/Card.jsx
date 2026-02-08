c853cbc33faee4f019c6d3ac74fbe12be15d0de4
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2026-02-08 20:44:07 +0100

    feat basic card for the characters

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,26 @@
 const Card = ({character}) => {
     return (
-        <h2 className="text-center text-white text-bold">
-            {character.fullName}
-        </h2>
+        <div className="flex flex-col items-center">
+            <div>
+                <img src={character.imageUrl} alt={`Retrato de ${character.firstName}`}/>
+            </div>
+            <div>
+                <div className="text-center">
+                    <h2 className="text-center text-white text-bold">
+                        {character.fullName}
+                    </h2>
+                    <h3 className="text-gray-400">
+                        {character.family}
+                    </h3>
+                    <h4 className="text-yellow-200">
+                        {character.title}
+                    </h4>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
     );
 };
 export default Card;
