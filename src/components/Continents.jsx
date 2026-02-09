bf6f364b0028e915b4e5594a08514ebcc12d167c
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2026-02-09 13:06:21 +0100

    map

--- src/components/Continents.jsx
+++ src/components/Continents.jsx
@@ -5,10 +5,10 @@ const Continents = () => {
     const [continents, setContinents] = useState([]);
 
     const positions = {
-        'Westeros': "top-[15%] left-[28%]",
-        'Essos': "top-[22%] left-[72%]",
-        'Sothoryos': "top-[78%] left-[58%]",
-        'Ulthos': "top-[62%] left-[48%]",
+        'Westeros': "top-[15%] left-[30%]",
+        'Essos': "top-[20%] left-[70%]",
+        'Sothoryos': "top-[80%] left-[60%]",
+        'Ulthos': "top-[60%] left-[50%]",
     };
 
     const fetchContinents = async () => {
@@ -20,25 +20,20 @@ const Continents = () => {
     fetchContinents(); }, [] );
 
     return (
-        <div className="flex flex-col items-center bg-black p-8 font-cinzel">
-            <div className="text-center mb-6">
-                <h2 className="text-2xl text-[#e6c300] tracking-[0.2em] font-bold uppercase border-b border-[#e6c300]/30 pb-2">
-                    Map of the Known World
-                </h2>
-            </div>
+        <div className="flex flex-col items-center bg-black p-16 font-cinzel">
 
-            <div className="relative w-full max-w-[1000px] border-2 border-[#e6c300]/50 rounded-sm shadow-[0_0_20px_rgba(230,195,0,0.2)] overflow-hidden">
+            <div className="relative w-full max-w-[1000px] border-2 border-yellow-400/50 rounded-sm shadow-[0_0_20px_rgba(230,195,0,0.2)] overflow-hidden">
                 <img 
                 src="/images/gameOfThronesMap.png" 
                 alt="Mapa de Poniente y Essos" 
-                className="w-full h-auto block grayscale-[20%] sepia-[10%]"
+                className="w-full h-auto block"
                 />
                 {continents.map((continent) => (
                     <div key={continent.id}
                         className={`absolute ${positions[continent.name]}`}
                     >
-                        <div className="bg-black/60 backdrop-blur-sm border border-[#e6c300]/60 px-4 py-2 rounded-sm shadow-xl transition-all hover:scale-110 hover:bg-black/80 cursor-default group">
-                            <span className="text-white text-sm md:text-lg font-bold tracking-widest uppercase group-hover:text-[#e6c300]">
+                        <div className="bg-black/60 border border-yellow-400 px-4 py-2 rounded-sm shadow-xl transition-all">
+                            <span className="text-white text-sm md:text-lg font-bold  uppercase">
                                 {continent.name}
                             </span>
                         </div>
